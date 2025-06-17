cf09e860aa5f66445d17331bdc899d7897f42d2b
Vana2Tired2Live <vanvilltheartist@gmail.com>
2025-06-17 10:51:13 +0800

    Add files via upload

--- Modul 4 (ORDER BY, Fungsi Agregasi, GROUP BY, HAVING)/Modul 4 (ORDER BY, Fungsi Agregasi, GROUP BY, HAVING).sql
+++ Modul 4 (ORDER BY, Fungsi Agregasi, GROUP BY, HAVING)/Modul 4 (ORDER BY, Fungsi Agregasi, GROUP BY, HAVING).sql
@@ -88,3 +88,89 @@ ORDER BY diskon_persen DESC;
 
 --------------------------------------------------------------------------------------
 
+-- Fungsi Agregasi
+-- Digunakan untuk mengolah beberapa barisan/row data untuk menghasilkan nilai baru
+-- Praktik 7
+-- Hitung atau jumlahkan seluruh nilai dari baris qty pada tabel penjualan
+SELECT qty FROM tr_penjualan;
+SELECT SUM(qty) FROM tr_penjualan;
+
+-- Praktik 8
+-- Hitung seluruh baris pada tabel penjualan
+SELECT * FROM tr_penjualan;
+SELECT COUNT(*) FROM tr_penjualan;
+
+-- Praktik 9
+-- Menggunakan 2 fungsi agregasi dalam satu query sql
+SELECT SUM(qty), COUNT(*) FROM tr_penjualan;
+
+-- Praktik 10
+-- Hitung rata-rata penjualan, penjualan tertinggi per transaksi, dan penjualan terrendah per transaksi
+SELECT AVG(qty), MAX(qty), MIN(qty)
+FROM tr_penjualan;
+
+-- Praktik 11
+-- Mengkombinasikan fungsi agregasi dengan kolom tertentu (nama_produk, kolom tgl_transaksi)
+SELECT tgl_transaksi, nama_produk, MAX(qty) FROM tr_penjualan;
+
+-- COUNT(DISTINCT)
+-- Digunakan untuk menghitung nilai yang unik saja
+-- Menghitung nilai unik yang ada pada nama produk di tabel penjualan
+-- Praktik 12
+-- Tanpa Distinct
+SELECT COUNT(nama_produk) FROM tr_penjualan;
+-- Dengan Distinct
+SELECT COUNT(DISTINCT nama_produk) FROM tr_penjualan;
+SELECT COUNT(nama_produk), COUNT(DISTINCT nama_produk) FROM tr_penjualan;
+
+--------------------------------------------------------------------------------------
+
+-- GROUP BY
+-- Mengelompokkan isi data dari satu atau beberapa kolom, biasanya digabung dengan fungsi agregasi
+-- GROUP BY 1 kolom
+-- Praktik 14
+-- Mengelompokkan nilai nama produk pada tabel penjualan
+SELECT nama_produk FROM tr_penjualan GROUP BY nama_produk;
+SELECT nama_produk FROM tr_penjualan;
+
+-- Praktik 15
+-- Mengelompokkan nilai nama produk dan qty pada tabel penjualan
+SELECT nama_produk, qty FROM tr_penjualan GROUP BY nama_produk, qty;
+
+-- Praktik 16
+-- Ambil qty dari nama produk berdasarkan hasil pengelompokan nama produk
+SELECT nama_produk, SUM(qty) FROM tr_penjualan GROUP BY nama_produk;
+
+-- Praktik 17
+-- Ambil jumlah qty dari namaproduk berdasarkan hasil pengelompokan nama produk urut dari besar ke kecil
+SELECT nama_produk, SUM(qty) FROM tr_penjualan GROUP BY nama_produk ORDER BY SUM(qty) DESC;
+
+-- Praktik 18
+-- Ambil jumlah qty dari nama produk yang jumlah qty > dari 2 berdasarkan hasil pengelompokkan nama produk
+SELECT nama_produk, SUM(qty)
+FROM tr_penjualan
+GROUP BY nama_produk
+HAVING SUM(qty) > 2
+ORDER BY SUM(qty) DESC;
+
+--------------------------------------------------------------------------------------
+
+-- Latihan Mandiri
+-- Produk dengan total jumlah terjual per kelompok nama produk di atas 4
+SELECT nama_produk, SUM(qty) AS total_terjual
+FROM tr_penjualan
+GROUP BY nama_produk
+HAVING total_terjual > 4;
+
+-- Produk dengan total jumlah terjual per kelompok nama produk sama dengan 9
+SELECT nama_produk, SUM(qty) AS total_terjual
+FROM tr_penjualan
+GROUP BY nama_produk
+HAVING total_terjual = 9;
+
+-- Kelompok nama produk dan total nilai penjualan (harga * jumlah - diskon), diurutkan dari terbesar
+SELECT nama_produk, SUM((harga*qty) - (diskon_persen/100*harga)) AS total_penjualan
+FROM tr_penjualan
+GROUP BY nama_produk
+ORDER BY total_penjualan DESC;
+
